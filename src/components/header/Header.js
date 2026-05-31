631ef6987c6afa073489afeb37bd7ba5aa2789e0
Abhishek <kailasj281@gmail.com>
2026-06-01 00:24:44 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -27,10 +27,19 @@ function Header() {
   return (
     <Headroom>
       <header className={isDark ? "dark-menu header" : "header"}>
-        <a href="/" className="logo">
-          <span className="grey-color"> &lt;</span>
-          <span className="logo-name">{greeting.username}</span>
-          <span className="grey-color">/&gt;</span>
+        <a href="/" className="logo" style={{ textDecoration: "none" }}>
+          {/* Replaced the brackets with a clean, modern all-caps sans-serif style */}
+          <span 
+            className="logo-name" 
+            style={{ 
+              fontFamily: "inherit", 
+              fontWeight: "700", 
+              letterSpacing: "1px",
+              textTransform: "uppercase"
+            }}
+          >
+            {greeting.username}
+          </span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label
