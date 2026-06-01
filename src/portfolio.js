72dcf0679e74a65e1b798df82e27e5dffa2883aa
Abhishek <kailasj281@gmail.com>
2026-06-01 12:33:13 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -89,7 +89,7 @@ const educationInfo = {
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: false, //Set it to true to show Proficiency Section
   experience: [
     {
       Stack: "Swift & SwiftUI Architecture",
