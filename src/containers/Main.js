84dca3801df71e6c4da488726d79c7b7a3be726c
Abhishek <kailasj281@gmail.com>
2026-06-01 11:39:57 +0530

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
-import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
