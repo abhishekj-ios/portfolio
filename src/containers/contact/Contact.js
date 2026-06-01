de5b7416b59c341b450ee0446c56cc8fe59575aa
Abhishek <kailasj281@gmail.com>
2026-06-02 00:46:10 +0530

    Update Contact.js

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -7,6 +7,7 @@ import email from "../../assets/lottie/email";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
+
 export default function Contact() {
   const {isDark} = useContext(StyleContext);
 
