1fcbf3f6f5ca78410a521087a385520873c38f64
Abhishek <kailasj281@gmail.com>
2026-06-02 14:05:21 +0530

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -62,7 +62,6 @@ const Main = () => {
             <Talks />
             <Twitter />
             <Podcast />
-            <Profile />
             <Footer />
             <ScrollToTopButton />
           </>
