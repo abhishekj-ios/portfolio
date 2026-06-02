0d0f86785eafa75956bc0d8b598524ccbd600255
Abhishek <kailasj281@gmail.com>
2026-06-02 18:50:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import AllApps from "./AllApps";
 
 function App() {
+  // 'page' state tracks which screen to show. 
+  // 'home' is default.
+  const [page, setPage] = useState("home");
+
   return (
     <div>
-      <Main />
+      {page === "home" ? (
+        // Pass a function to Main so the "All Projects" button can change the page
+        <Main onNavigate={(target) => setPage(target)} />
+      ) : (
+        // Render AllApps and give it a way to go back home
+        <AllApps onNavigate={(target) => setPage(target)} />
+      )}
     </div>
   );
 }
