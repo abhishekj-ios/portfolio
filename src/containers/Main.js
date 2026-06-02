c9f91907fe38798b83c1d3b8448762ef2911847c
Abhishek <kailasj281@gmail.com>
2026-06-02 22:09:57 +0530

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -57,7 +57,7 @@ const Main = ({ setPage }) => {
             <WhatIDo />
             <StackProgress />
             <Education />
-            <Projects />
+            <Projects setPage={setPage} />
             <Experience />
             <Achievement />
             <Blogs />
