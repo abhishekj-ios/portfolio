04fd9ddcdcfcfee9a9432f4d3b72da3d798fc72d
Abhishek <kailasj281@gmail.com>
2026-06-03 15:29:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Main from "./containers/Main";
 import AllApps from "./AllApps"; 
-import Header from "./components/Header/Header"; // <--- ADD THIS IMPORT (Adjust path if needed)
+import Header from "./components/header/Header"; // <--- ADD THIS IMPORT (Adjust path if needed)
 
 function App() {
   const [page, setPage] = useState("home");
